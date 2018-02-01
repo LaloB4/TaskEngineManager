2e75f9f42608e62324f4ab50ea1dad748467def6
Eduardo.barrera <eduardo.barrera@itlocals-MacBook-Pro.local>
2018-02-01 11:49:05 -0600

    Initial app version

---
+++ src/main/java/com/eduardobarrera/taskenginemanager/TaskenginemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.eduardobarrera.taskenginemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskenginemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskenginemanagerApplication.class, args);
+	}
+}
